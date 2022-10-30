4e39ae71d0b36af566cf5f74609810e3ea510220
zbynekzundalek <zbynek.zundalek@gmail.com>
2022-10-30 22:51:54 +0900

    Changed welcome message

--- mod.ts
+++ mod.ts
@@ -1,3 +1,3 @@
 import { serve } from "https://deno.land/std@0.155.0/http/server.ts";
 
-serve((_req: Request) => new Response("Hello World"));
+serve((_req: Request) => new Response("Hello World from Deno"));
