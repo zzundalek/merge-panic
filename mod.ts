cf230f585430ccc73aaa7e447482430d21bbba91
zbynekzundalek <zbynek.zundalek@gmail.com>
2022-11-03 02:47:49 +0900

    Added Oak and SSR

--- mod.ts
+++ mod.ts
@@ -1,3 +1,22 @@
-import { serve } from "https://deno.land/std@0.155.0/http/server.ts";
+import { Application, Router } from 'https://deno.land/x/oak@v11.1.0/mod.ts';
+import render from 'https://esm.sh/preact-render-to-string@5.2.6';
 
-serve((_req: Request) => new Response("Hello World from Deno"));
+import { Greet } from './greet.tsx';
+
+const router = new Router();
+router.get('/', (ctx) => {
+	ctx.response.body = `<!DOCTYPE html>
+    <html>
+      <head><title>Hello world!</title><head>
+      <body>
+        ${render(Greet('stranger'))}
+      </body>
+    </html>
+  `;
+});
+
+const app = new Application();
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+app.listen({ port: 80 });
