8a62016f3515f3260055923eb7e0f321fe703dff
zbynekzundalek <zbynek.zundalek@gmail.com>
2022-11-05 01:00:42 +0900

    Fixed title overflow

--- islands/Card.tsx
+++ islands/Card.tsx
@@ -11,12 +11,12 @@ export default function Card(props: CardProps) {
   const [likeCount, setLikeCount] = useState(0);
 
   return (
-    <div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+    <div class="max-w-md mx-auto my-2 bg-white rounded-xl hover:shadow-md hover:cursor-pointer overflow-hidden md:max-w-2xl">
       <div class="md:flex">
         <div class="p-8 relative">
           <a
             href="#"
-            class="block mt-1 text-lg leading-tight font-medium text-black hover:underline"
+            class="block mt-1 text-lg leading-tight font-medium text-black"
           >
             {props.title}
           </a>
--- routes/index.tsx
+++ routes/index.tsx
@@ -8,28 +8,32 @@ export default function Home() {
     desription:
       "This blog has been rendered serverside using Fresh and Deno. Try using like button.",
   };
+  const examples = [example, example, example, example, example];
 
   return (
     <>
       <Head>
         <title>Merge panic</title>
       </Head>
-      <div class="p-4 mx-auto max-w-screen-md">
-        <h1 class="text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight">
-          Merge panic blog
-        </h1>
+      <div class="lg:w-3/5 mx-auto px-2">
+        <div class="top-0 my-5">
+          <h1 class="text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight mb-2">
+            Merge panic
+          </h1>
+          <hr />
+        </div>
+        {examples.map((item) => <Card {...example} />)}
+        <div class="mx-auto my-5 w-max">
+          <a href="https://fresh.deno.dev">
+            <img
+              width="197"
+              height="37"
+              src="https://fresh.deno.dev/fresh-badge.svg"
+              alt="Made with Fresh"
+            />
+          </a>
+        </div>
       </div>
-      <div class="absolute top-3 right-2 ...">
-        <a href="https://fresh.deno.dev">
-          <img
-            width="197"
-            height="37"
-            src="https://fresh.deno.dev/fresh-badge.svg"
-            alt="Made with Fresh"
-          />
-        </a>
-      </div>
-      <Card {...example} />
     </>
   );
 }
